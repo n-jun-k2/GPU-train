31a5377d3de307273e01b4dcd03abe3a36dffb1f
remote-client <http.create.jp@gmail.com>
2020-08-30 13:30:52 +0900

    fix install scripts

--- scripts/ubuntu-1804/wsl-install-cuda10.1.sh
+++ scripts/ubuntu-1804/wsl-install-cuda10.1.sh
@@ -6,4 +6,4 @@ wget https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda-r
 dpkg -i cuda-repo-ubuntu1804-10-1-local-10.1.105-418.39_1.0-1_amd64.deb
 sudo apt-key add /var/cuda-repo-10-1-local-10.1.105-418.39/7fa2af80.pub
 sudo apt-get update
-apt-get install -y cuda-toolkit-10-1
+sudo apt-get install -y cuda-toolkit-10-1
